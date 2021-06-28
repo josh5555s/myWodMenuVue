202ba9b017f9ab64f241eeb19155993d198804d4
BuildTools <unconfigured@null.spigotmc.org>
2021-06-28 14:23:03 -0700

    init vuex

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import store from './store/index'
 
 import SelectStore from './components/SelectStore.vue'
 import SelectProduct from './components/SelectProduct.vue'
