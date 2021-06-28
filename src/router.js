c3d92ac09f510406d436faaed3519fc253b40fce
BuildTools <unconfigured@null.spigotmc.org>
2021-06-28 13:18:49 -0700

    modularize to router.js

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import SelectStore from './components/SelectStore.vue'
+import SelectProduct from './components/SelectProduct.vue'
+import ShowDisplay from './components/ShowDisplay.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: SelectStore },
+    { path: '/:store', component: SelectProduct },
+    { path: '/:store/:product', component: ShowDisplay },
+  ]
+})
+
+export default router
