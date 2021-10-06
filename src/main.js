f51f72ea0788f6dc720a9aefdfae4c9ac2b2bcaf
BuildTools <unconfigured@null.spigotmc.org>
2021-10-06 14:42:43 -0700

    google analytics with gtag library

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import VueGtag from "vue-gtag-next"
+
 
 const app = createApp(App)
+
+app.use(VueGtag, {
+  config: { id: "G-RBH5LRWBLJ" }
+})
 
 app.use(router)
 app.use(store)
